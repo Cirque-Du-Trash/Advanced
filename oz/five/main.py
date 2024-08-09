01aa61d3425840be4ec8aff640191c9ba04b1963
mr-cliche-git <mr.cliche7@gmail.com>
2024-08-09 16:02:28 +0900

    Ad

--- oz/five/main.py
+++ oz/five/main.py
@@ -5,7 +5,7 @@ import random
 import string
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql.sqltypes import String, Integer, Float, Date, DateTime, Boolean, SmallInteger
+from sqlalchemy.sql.sqltypes import String, Integer, Float, Date, DateTime, Boolean, SmallInteger, DECIMAL, Enum, Time
 from faker import Faker
 
 # 설정 파일 로드
@@ -25,11 +25,11 @@ def generate_fixed_length_string(length):
 
  # 유니크 문자열 생성 함수
 def generate_unique_string(existing_values, length=10):
-    while True:
-        unique_str = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-        if unique_str not in existing_values:
-            return unique_str
-    
+    existing_values_lower = {v.lower() for v in existing_values}
+    unique_str = generate_fixed_length_string(length)
+    while unique_str.lower() in existing_values_lower:
+        unique_str = generate_fixed_length_string(length)
+    return unique_str
 
  # 유니크 인티저 생성 함수
 def generate_unique_integer(existing_values, min_value=0, max_value=10000):
@@ -41,8 +41,12 @@ def generate_unique_integer(existing_values, min_value=0, max_value=10000):
 # 컬럼 타입에 따른 더미 데이터 생성기
 def generate_column_data(col_type, existing_values):
     faker = Faker()
+
+    if isinstance(col_type, Enum):
+        enum_values = col_type.enums
+        return random.choice(enum_values)
     
-    if isinstance(col_type, String):
+    elif isinstance(col_type, String):
         length = col_type.length
         if length and existing_values is not None:
             return generate_unique_string(existing_values, length)
@@ -51,8 +55,13 @@ def generate_column_data(col_type, existing_values):
     elif isinstance(col_type, Integer) or isinstance(col_type, Float) or isinstance(col_type, SmallInteger):
         if existing_values is not None:
             return generate_unique_integer(existing_values)
+        if 'TINYINT' in str(col_type):
+            return random.choice([0, 1])
         return faker.random_int(min=0, max=10000)
     
+    elif isinstance(col_type, DECIMAL):
+        return faker.latitude()  # for latitude and longitude fields
+
     elif isinstance(col_type, Boolean):
         return faker.boolean()
     
@@ -62,31 +71,47 @@ def generate_column_data(col_type, existing_values):
     elif isinstance(col_type, DateTime):
         return faker.date_time()
     
+    elif isinstance(col_type, Time):
+        return faker.time()
+    
     else:
-        return None  # 또는 기본값으로 채울 수 있음
+        return faker.text()
 
 
 # 테이블에 맞는 더미 데이터 생성기
 def generate_dummy_data(table, num_rows):
     data = []
     existing_values = {col.name: set() for col in table.columns}
+
+    inserted_count = 0  # 데이터 삽입 카운트
     
+    # 유니크 인덱스가 있는 컬럼 찾기
+    unique_columns = set()
+
+    for col in table.primary_key.columns:
+        unique_columns.add(col.name)
+
+    for index in table.indexes:
+        if index.unique:
+            unique_columns.update(col.name for col in index.columns)
+
     for _ in range(num_rows):
         row = {}
         for col in table.columns:
-            # 유니크 제약 조건이 있는 컬럼의 경우, 기존 값을 참조하여 유니크한 값을 생성
-            print(col.constraints)
-            print(existing_values)
-            if any(constraint.name == 'uq_' + col.name for constraint in table.constraints):
+            if col.name in unique_columns:
+                # 유니크 제약 조건이 있는 컬럼에 대해 기존 값을 참조하여 유니크한 값을 생성
                 row[col.name] = generate_column_data(col.type, existing_values[col.name])
             else:
                 row[col.name] = generate_column_data(col.type, None)
-            
                 
             existing_values[col.name].add(row[col.name])
         data.append(row)
-    return data
 
+        inserted_count += 1
+        
+        # 데이터 생성 후 카운트 출력
+        print(f"Generated {inserted_count} rows for table '{table.name}'")
+    return data
 
 # 데이터베이스 연결 설정 및 데이터 삽입
 def insert_data(config):
@@ -108,7 +133,7 @@ def insert_data(config):
         
         dummy_data = generate_dummy_data(table, num_rows)
         session.execute(table.insert(), dummy_data)
-    
+
     session.commit()
     session.close()
 
