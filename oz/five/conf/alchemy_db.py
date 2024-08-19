cbddbb5d0e91c5d8eb5428f338b2ade1d06c3286
mr-cliche-git <mr.cliche7@gmail.com>
2024-08-19 18:10:15 +0900

    MODULARIZATION

--- oz/five/conf/alchemy_db.py
+++ oz/five/conf/alchemy_db.py
@@ -1,7 +1,9 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
+from conf.app_config import db_username, db_password, db_host, db_port, db_name
 
-SQLALCHEMY_DATABASE_URL_DUMMY = 'mysql+pymysql://root:root@localhost:3306/airportdb'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL_DUMMY, echo=False)
-SessionLocalDummy = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}")
+metadata = MetaData()
+metadata.reflect(bind=engine)
+    
+Session = sessionmaker(bind=engine)
--- oz/five/database.py
+++ oz/five/database.py
@@ -1,17 +1,11 @@
-from sqlalchemy import create_engine, MetaData, Table, text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Table, text
 from data_generator import generate_dummy_data
-from conf.app_config import db_username, db_password, db_host, db_port, db_name
+from conf.alchemy_db import engine, metadata, Session
+
+session = Session()
 
 # 데이터베이스 연결 설정 및 데이터 삽입
 def insert_data(config):
-    engine = create_engine(f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}")
-    metadata = MetaData()
-    metadata.reflect(bind=engine)
-    
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    
     for table_name, table_config in config['tables'].items():
         table = Table(table_name, metadata, autoload_with=engine)
         
